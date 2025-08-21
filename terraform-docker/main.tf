e2871ae981d592c1a25f3dc57a55ef9f93cc3d69
vagrant <vagrant@devops-lab>
2025-08-21 05:34:05 +0000

    added

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -11,13 +11,27 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
+# resource "docker_image" "quiz_app" {
+#   name = "quiz-app:${filesha256("${path.module}/../k8s-quiz/Dockerfile")}"
+#   build {
+#     context    = "/home/vagrant/terraform-docker/k8s-quiz"
+#     dockerfile = "Dockerfile"
+#   }
+# }
+
+
 resource "docker_image" "quiz_app" {
-  name = "quiz-app:latest"
+  name = "quiz-app:${filesha256("${path.module}/../k8s-quiz/Dockerfile")}"
   build {
-    context    = "/home/vagrant/terraform-docker/k8s-quiz"
+    context    = "${path.module}/../k8s-quiz"
     dockerfile = "Dockerfile"
   }
+
+  triggers = {
+    app_code = filesha256("${path.module}/../k8s-quiz/app.py")
+  }
 }
+
 
 resource "docker_container" "quiz_app" {
   name  = "quiz_app"
